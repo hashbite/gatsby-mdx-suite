ee530dc4eb389c65ed103836d4193fa9165e7c88
Benedikt Rötsch <opensource@axe312.dev>
2019-09-05 23:18:26 +0200

    add todo

--- demo/gatsby-browser.js
+++ demo/gatsby-browser.js
@@ -8,6 +8,7 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 // Ensure user gets the latest content as soon updates are ready
 // Might prevent: https://github.com/gatsbyjs/gatsby/issues/13410
+// @todo really still needed with offline plugin v3? this can be pretty annoying
 export const onServiceWorkerUpdateReady = () => window.location.reload(true)
 
 export const wrapRootElement = ({ element }) => {
