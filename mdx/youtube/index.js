bca4f2a156f236e1634ca8f7212bd84ffe7ade2d
axe312ger <opensource@axe312.dev>
2019-10-17 17:00:12 +0200

    rewrite youtube feed to follow new mdx data context structure

--- mdx/youtube/index.js
+++ mdx/youtube/index.js
@@ -1,1 +1,20 @@
 // noop
+
+export const gatsbyMdxSuiteYoutube = graphql`
+  fragment GatsbyMdxSuiteYoutubeFeed on YoutubeVideo {
+    channelId
+    videoId
+    title
+    publishedAt
+    localThumbnail {
+      childImageSharp {
+        sqip(mode: 4, numberOfPrimitives: 12) {
+          dataURI
+        }
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid_withWebp_noBase64
+        }
+      }
+    }
+  }
+`
