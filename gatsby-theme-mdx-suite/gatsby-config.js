e11bbc5d7b268df29b20858749209934e7fe49ce
axe312ger <opensource@axe312.dev>
2019-10-22 17:37:17 +0200

    Revert "fix(build): temporary disable docz generation till https://github.com/doczjs/docz/pull/1188 is released"
    This reverts commit f81746ea7953ec695e6db871593ba6196bab424d.

--- gatsby-theme-mdx-suite/gatsby-config.js
+++ gatsby-theme-mdx-suite/gatsby-config.js
@@ -1,16 +1,17 @@
-// const path = require('path')
-// const src = path.resolve(process.cwd(), 'node_modules/@gatsby-mdx-suite')
+const path = require('path')
+const src = path.resolve(process.cwd(), 'node_modules/@gatsby-mdx-suite')
 
 module.exports = ({ mdx }) => ({
   plugins: [
     'gatsby-plugin-theme-ui',
-    // {
-    //   resolve: `gatsby-theme-docz`,
-    //   options: {
-    //     src: src,
-    //     files: `**/docs/*.{md,markdown,mdx}`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-theme-docz`,
+      options: {
+        filterComponents: (modules) => modules,
+        src: src,
+        files: `**/docs/*.{md,markdown,mdx}`,
+      },
+    },
     {
       resolve: `gatsby-plugin-mdx`,
       options: mdx,
