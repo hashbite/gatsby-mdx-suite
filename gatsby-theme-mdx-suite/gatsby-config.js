8faef2d06b70c648afcacea13a44113b0e22d40e
axe312ger <opensource@axe312.dev>
2019-11-12 14:40:39 +0100

    feat(config): allow passing of custom docz src folder

--- gatsby-theme-mdx-suite/gatsby-config.js
+++ gatsby-theme-mdx-suite/gatsby-config.js
@@ -1,14 +1,17 @@
 const path = require('path')
-const src = path.resolve(process.cwd(), 'node_modules/@gatsby-mdx-suite')
+const defaultDoczSrc = path.resolve(
+  process.cwd(),
+  'node_modules/@gatsby-mdx-suite'
+)
 
-module.exports = ({ mdx }) => ({
+module.exports = ({ mdx, doczSrc }) => ({
   plugins: [
     'gatsby-plugin-theme-ui',
     {
       resolve: `gatsby-theme-docz`,
       options: {
         filterComponents: (modules) => modules,
-        src: src,
+        src: doczSrc || defaultDoczSrc,
         files: `**/docs/*.{md,markdown,mdx}`,
       },
     },
