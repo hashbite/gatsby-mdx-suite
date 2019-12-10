f1056efe584b46ac697aa99da2266bc42be8b2bd
Benedikt Rötsch <opensource@axe312.dev>
2019-12-10 16:28:46 +0100

    feat(Documentation): do not render docs on production for build and client performance reasons

--- gatsby-theme-mdx-suite/gatsby-config.js
+++ gatsby-theme-mdx-suite/gatsby-config.js
@@ -1,10 +1,15 @@
-module.exports = ({ mdx, doczSrc }) => ({
+const isProduction = process.env.NODE_ENV === 'production'
+const isStaging = !!process.env.STAGING
+
+const renderDocs = isStaging || !isProduction
+
+module.exports = ({ mdx, doczSrc, forceDocs = false }) => ({
   plugins: [
     // Styling
     'gatsby-plugin-theme-ui',
     `gatsby-plugin-emotion`,
     // Docs
-    {
+    (forceDocs || renderDocs) && {
       resolve: `gatsby-theme-docz`,
       options: {
         filterComponents: (modules) => modules,
