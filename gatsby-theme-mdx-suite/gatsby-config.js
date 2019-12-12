b6243df044ec85d105f6441b59431d07634e0c5e
Benedikt Rötsch <opensource@axe312.dev>
2019-12-12 23:19:05 +0100

    fixup! WIP

--- gatsby-theme-mdx-suite/gatsby-config.js
+++ gatsby-theme-mdx-suite/gatsby-config.js
@@ -1,6 +1,6 @@
-// const annotationResolver = require('./resolver')
+// const annotationResolver = require(`./resolver`)
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === `production`
 const isStaging = !!process.env.STAGING
 
 const renderDocs = isStaging || !isProduction
@@ -10,14 +10,14 @@ const renderDocs = isStaging || !isProduction
 module.exports = ({ mdx, doczSrc, forceDocs = false }) => ({
   plugins: [
     // Styling
-    'gatsby-plugin-theme-ui',
+    `gatsby-plugin-theme-ui`,
     `gatsby-plugin-emotion`,
     // Docs
     (forceDocs || renderDocs) && {
       resolve: `gatsby-theme-docz`,
       options: {
-        filterComponents: (modules) => modules,
-        // src: tmpSrc || doczSrc || 'src',
+        // filterComponents: (modules) => modules,
+        // src: tmpSrc || doczSrc || `src`,
         files: [
           `src/components/mdx/**/docs/*.{md,markdown,mdx}`,
           `node_modules/@gatsby-mdx-suite/*/docs/*.{md,markdown,mdx}`,
@@ -26,9 +26,9 @@ module.exports = ({ mdx, doczSrc, forceDocs = false }) => ({
         noRootRoute: true,
         // docgenConfig: { resolver: annotationResolver },
         customPattern: [
-          'src/components/mdx/**/*.{js,jsx,mjs}',
-          '../../node_modules/@gatsby-mdx-suite/**/*.{js,jsx,mjs}',
-          '!**/doczrc.js',
+          `src/components/mdx/**/*.{js,jsx,mjs}`,
+          `node_modules/@gatsby-mdx-suite/**/*.{js,jsx,mjs}`,
+          `../../node_modules/@gatsby-mdx-suite/**/*.{js,jsx,mjs}`,
         ],
       },
     },
