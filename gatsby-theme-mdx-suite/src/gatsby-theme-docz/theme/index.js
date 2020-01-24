c4794a5c5773b6ce89db9c979ba9f367aaa6a8ed
Benedikt Rötsch <opensource@axe312.dev>
2020-01-24 15:21:09 +0100

    fix(docs): workaround to ensure playgrounds render.. in most cases...

--- gatsby-theme-mdx-suite/src/gatsby-theme-docz/theme/index.js
+++ gatsby-theme-mdx-suite/src/gatsby-theme-docz/theme/index.js
@@ -3,4 +3,4 @@ import { merge } from 'lodash/fp'
 import baseTheme from 'gatsby-theme-docz/src/theme/index'
 import mdxTheme from '../../gatsby-plugin-theme-ui/index'
 
-export default merge(baseTheme, mdxTheme)
+export default merge(merge(baseTheme, mdxTheme), baseTheme.prism.light)
