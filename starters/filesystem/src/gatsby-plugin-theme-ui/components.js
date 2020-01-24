9358c1be72de19ee394438851a9fac833200d616
Benedikt Rötsch <opensource@axe312.dev>
2020-01-24 19:15:12 +0100

    refactor(starters): remove umaintained filesystem starter for now

--- starters/filesystem/src/gatsby-plugin-theme-ui/components.js
+++
@@ -1,9 +0,0 @@
-import Grid from '@gatsby-mdx-suite/mdx-layout/grid'
-import Viewport from '@gatsby-mdx-suite/mdx-layout/viewport'
-import Image from '@gatsby-mdx-suite/mdx-basic/gatsby-image'
-
-export default {
-  Grid,
-  Viewport,
-  Image,
-}
--- starters/filesystem/src/gatsby-plugin-theme-ui/typography.js
+++
@@ -1,4 +0,0 @@
-import { toTheme } from '@theme-ui/typography'
-import grandView from 'typography-theme-grand-view'
-
-export default toTheme(grandView)
