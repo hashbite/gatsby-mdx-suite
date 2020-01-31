b87202ef49bae29205b0ce325d3434ded786eec0
Benedikt Rötsch <opensource@axe312.dev>
2020-01-31 21:40:30 +0100

    refactor(menu): remove hiddenOnMobile attribute

--- packages/menu/menu-li.js
+++ packages/menu/menu-li.js
@@ -4,22 +4,13 @@ import { css } from '@emotion/core'
 import MenuTitle from './menu-title'
 
 const MenuLi = styled.li(
-  ({ theme, active, hiddenOnMobile }) => css`
+  ({ theme, active }) => css`
     padding: 0 ${theme.spacing.s1}px;
 
     ${active &&
       css`
         ${MenuTitle} {
           font-weight: bold;
-        }
-      `}
-    ${hiddenOnMobile &&
-      css`
-        ${MenuTitle} {
-          display: none;
-          @media (min-width: ${theme.breakpoints[2]}) {
-            display: block;
-          }
         }
       `}
   `
