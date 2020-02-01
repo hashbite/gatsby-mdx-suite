9584dc0d1de6f03b822b4309d444466208cbe90a
Benedikt Rötsch <opensource@axe312.dev>
2020-02-01 23:57:16 +0100

    fix(starters/contentful): readd functionallity to header

--- starters/contentful/src/components/mdx/header.js
+++ starters/contentful/src/components/mdx/header.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import propTypes from 'prop-types'
+import styled from '@emotion/styled'
 
-const Header = ({ children }) => {
-  return <header>{children}</header>
+import MenuLevel from '@gatsby-mdx-suite/menu/menu-level'
+import LanguageSwitch from '@gatsby-mdx-suite/i18n/language-switch'
+import { centerToContentColumn } from '@gatsby-mdx-suite/helpers'
+import Image from '@gatsby-mdx-suite/mdx-basic/image'
+
+import ColorModeSwitch from '../color-mode-switch'
+
+const HeaderContent = styled.div`
+  ${centerToContentColumn}
+`
+
+const Header = ({ children, backgroundImageId }) => {
+  return (
+    <header>
+      <MenuLevel rootMenuItemId="6Id378BoElgMsJJd81IyP3" />
+      <LanguageSwitch />
+      <ColorModeSwitch />
+      <HeaderContent>{children}</HeaderContent>
+      {backgroundImageId && <Image id={backgroundImageId} />}
+    </header>
+  )
 }
 
 Header.propTypes = {
   children: propTypes.node,
+  backgroundImageId: propTypes.string,
 }
 
 export default Header
