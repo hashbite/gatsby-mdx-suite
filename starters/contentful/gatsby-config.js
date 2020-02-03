b558d80e8b738cbdca099792a90860385d947c9b
Benedikt Rötsch <opensource@axe312.dev>
2020-02-04 00:07:42 +0100

    docs(starters/contentful): add notes to gatsby-config.js

--- starters/contentful/gatsby-config.js
+++ starters/contentful/gatsby-config.js
@@ -6,26 +6,36 @@ require('dotenv').config({
 })
 
 module.exports = {
-  pathPrefix: `/gatsby-mdx-suite`,
+  pathPrefix: ``,
   siteMetadata: {
-    title: `Gatsby MDX Suite Contentful`,
-    description: `Gatsby MDX Suite starter using Contentful`,
+    title: `Gatsby MDX Suite`,
+    description: `Get started with MDX & Contentful`,
     siteUrl: `https://axe312ger.github.io/gatsby-mdx-suite`,
   },
   plugins: [
     {
       resolve: `gatsby-theme-mdx-suite`,
       options: {
+        // List your supported locales
         langs: ['en-US', 'de'],
         defaultLocale: 'en-US',
+        // This maps locales to path prefixes
         localeMap: {
           'en-US': '',
           de: 'de',
         },
+        /**
+         * Disabling i18n:
+         * 1. Use a single locale in "langs" array config: ['en']
+         * 2. Map it in localeMap to an empty string: { en: '' }
+         */
+        // This maps content types to path prefixes.
         pageTypeMap: {
           page: null,
           blogPost: 'blog',
         },
+        // These will be passed to i18next.
+        // You may consider haveing these in a seperate json file and import it.
         translations: {
           'en-US': {
             translation: {
@@ -42,6 +52,21 @@ module.exports = {
             },
           },
         },
+        /**
+         * Configuratoin for automatic media usage detection within your MDX content.
+         *
+         * This configures the media field for all mdx fields
+         * For each collection the raw mdx source will be searched for media assets
+         * Components are located via an query selector the id extracted from a given attribute
+         * Each found media asset will be added as reference, grouped by the given collection name
+         *
+         * Why?
+         *
+         * This allows you to generate different image presets with Gatsby:
+         * * Editors can freely add images to the content
+         * * No extra reference field is need to locate used images
+         * * You can even select nested components when rendering more complex content.
+         */
         mediaCollections: {
           background: {
             selector:
@@ -71,6 +96,7 @@ module.exports = {
         icon: `src/assets/icon.svg`,
       },
     },
+    // Duplicate and rename .env.example to .env and fill in your credentials
     {
       resolve: `gatsby-source-contentful`,
       options: {
