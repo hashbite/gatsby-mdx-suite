e7189f774ac98f5de3f0e30f79f02c827b1132a0
Benedikt Rötsch <opensource@axe312.dev>
2020-02-04 16:32:08 +0100

    fix(docs): remove docz as it causes problems with our own MDX rendering

--- gatsby-theme-mdx-suite/src/gatsby-theme-docz/theme/index.js
+++
@@ -1,6 +0,0 @@
-import { merge } from 'lodash/fp'
-
-import baseTheme from 'gatsby-theme-docz/src/theme/index'
-import mdxTheme from '../../gatsby-plugin-theme-ui/index'
-
-export default merge(merge(baseTheme, mdxTheme), baseTheme.prism.light)
