355583d965bb110106eb1a8f6b861d15af4259fd
Benedikt Rötsch <opensource@axe312.dev>
2020-02-04 16:32:08 +0100

    fix(docs): remove docz as it causes problems with our own MDX rendering

--- gatsby-theme-mdx-suite/gatsby-config.js
+++ gatsby-theme-mdx-suite/gatsby-config.js
@@ -3,31 +3,11 @@ const isStaging = !!process.env.STAGING
 
 const renderDocs = isStaging || !isProduction
 
-module.exports = ({ mdx, doczSrc, forceDocs = false }) => ({
+module.exports = ({ mdx, forceDocs = false }) => ({
   plugins: [
     // Styling
     `gatsby-plugin-theme-ui`,
     `gatsby-plugin-emotion`,
-    // Docs
-    (forceDocs || renderDocs) && {
-      resolve: `gatsby-theme-docz`,
-      options: {
-        filterComponents: (modules) => modules,
-        files: [
-          `src/components/mdx/**/docs/*.{md,markdown,mdx}`,
-          `node_modules/@gatsby-mdx-suite/*/docs/*.{md,markdown,mdx}`,
-          `../../node_modules/@gatsby-mdx-suite/*/docs/*.{md,markdown,mdx}`,
-        ],
-        noRootRoute: true,
-        docgenConfig: {
-          searchPatterns: [
-            `src/components/mdx/**/*.{js,jsx,mjs}`,
-            `node_modules/@gatsby-mdx-suite/**/*.{js,jsx,mjs}`,
-            `../../node_modules/@gatsby-mdx-suite/**/*.{js,jsx,mjs}`,
-          ],
-        },
-      },
-    },
     // MDX
     {
       resolve: `gatsby-plugin-mdx`,
