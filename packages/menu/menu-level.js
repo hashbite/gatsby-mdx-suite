12eacee601d2fea81eb1f2edcc257b8b59f430f1
Benedikt Rötsch <opensource@axe312.dev>
2020-02-04 16:13:29 +0100

    fix(menu): context data is now accessed properly

--- packages/menu/menu-level.js
+++ packages/menu/menu-level.js
@@ -34,11 +34,12 @@ export default function MenuLevel({ rootMenuItemId, level = 1 }) {
   `)
 
   const {
-    pageContext: { pageId, locale: activeLocale },
+    pageContext: { pageId: activePageId, locale: activeLocale },
   } = useContext(MdxSuiteContext)
 
   const menuRoot = queryResult.allContentfulMenuItem.nodes.find(
-    ({ locale, pageId }) => pageId === rootMenuItemId && locale === activeLocale
+    ({ locale, pageId }) =>
+      pageId === rootMenuItemId && (!activeLocale || locale === activeLocale)
   )
 
   if (!menuRoot) {
@@ -46,7 +47,7 @@ export default function MenuLevel({ rootMenuItemId, level = 1 }) {
   }
 
   const activeTrail = findActiveTrail({
-    pageId,
+    activePageId,
     subTree: [menuRoot],
   })
 
