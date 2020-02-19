7f2b502b2106b8fc48479267f65bd341cbd8c484
Benedikt Rötsch <opensource@axe312.dev>
2020-02-19 18:09:26 +0100

    fix(assets): add locale to asset fragment

--- gatsby-theme-mdx-suite/src/graphql-fragments/contentful-assets.js
+++ gatsby-theme-mdx-suite/src/graphql-fragments/contentful-assets.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 export const MdxSuiteContentfulAsset = graphql`
   fragment MdxSuiteContentfulAsset on ContentfulAsset {
     assetId: contentful_id
+    locale: node_locale
     title
     description
     file {
