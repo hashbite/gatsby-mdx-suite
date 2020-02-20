a036694e459bae2cdd240323cbe766dbf12bd642
Benedikt Rötsch <opensource@axe312.dev>
2020-02-20 15:17:56 +0100

    default config

--- starters/contentful/tailwind.config.js
+++ starters/contentful/tailwind.config.js
@@ -11,4 +11,24 @@ console.log('---')
 
 // module.exports = toTailwind(theme)
 
-module.exports = {}
+module.exports = {
+  important: true,
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      },
+      margin: {
+        '96': '24rem',
+        '128': '32rem',
+      },
+    },
+  },
+  variants: {
+    opacity: ['responsive', 'hover'],
+  },
+}
