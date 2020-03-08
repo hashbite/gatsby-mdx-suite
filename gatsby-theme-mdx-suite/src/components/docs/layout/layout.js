d057e078d5951a3122066ee6891ad9ecb8514c61
Benedikt Rötsch <opensource@axe312.dev>
2020-03-08 05:04:55 +0100

    feat(docs): add mobile menu and entypo icon set
    closes #77
    closes #69

--- gatsby-theme-mdx-suite/src/components/docs/layout/layout.js
+++ gatsby-theme-mdx-suite/src/components/docs/layout/layout.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'gatsby-link'
 import propTypes from 'prop-types'
 import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 import { Styled } from 'theme-ui'
 import tw from 'twin.macro'
 import 'tailwindcss/dist/base.css'
+
+import { EntypoMenu } from 'react-entypo-icons'
 
 const DocsWrapper = styled.div`
   ${tw`grid h-screen w-screen`}
@@ -16,7 +19,10 @@ const DocsWrapper = styled.div`
 
 const LayoutHeader = styled.div`
   ${tw`
-    px-4 flex justify-between items-center
+    fixed left-0 right-0
+    md:static
+    p-4 flex justify-between items-center flex-wrap
+    md:py-0 md:flex-no-wrap
     bg-gray-200 shadow-lg
   `}
   grid-area: head;
@@ -24,11 +30,30 @@ const LayoutHeader = styled.div`
 
 const DocsTitle = tw(Styled.h1)`m-0 text-2xl`
 
-const MenuLink = tw(Link)`px-4`
+const MenuLink = tw(Link)`my-2 md:my-0 md:px-2 lg:px-4`
 
-const Menu = tw.nav`flex content-center`
+const Menu = styled.nav(
+  ({ menuOpen }) => css`
+    ${tw`
+    py-1 w-full flex-grow
+    md:py-0 md:w-auto md:flex-auto
+    flex flex-col content-center
+    md:flex-initial md:flex-row
+    md:text-sm lg:text-base
+  `}
+    ${!menuOpen && tw`hidden md:block`}
+  `
+)
+
+const MenuToggleWrapper = tw.div`p-1 md:hidden`
+const MenuToggle = tw(EntypoMenu)`cursor-pointer`
 
 const Layout = ({ children, title }) => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleMenuToggleClick = () => {
+    setMenuOpen(!menuOpen)
+  }
   return (
     <Styled.root>
       <DocsWrapper>
@@ -37,7 +62,13 @@ const Layout = ({ children, title }) => {
             <Link to="/docs">Docs</Link>
             {title && ` - ${title}`}
           </DocsTitle>
-          <Menu>
+          <MenuToggleWrapper>
+            <MenuToggle
+              onClick={handleMenuToggleClick}
+              style={{ verticalAlign: 'middle' }}
+            />
+          </MenuToggleWrapper>
+          <Menu menuOpen={menuOpen}>
             <MenuLink to="/docs/editing-basics">Editing Basics</MenuLink>
             <MenuLink to="/docs/playground">Playground</MenuLink>
             <MenuLink to="/docs/kitchen-sink">Kitchen Sink</MenuLink>
