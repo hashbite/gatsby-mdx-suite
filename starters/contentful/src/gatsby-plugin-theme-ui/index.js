6c16a81c778cc360fc0a6fa2bc5b7d19c33e1e4e
Benedikt Rötsch <opensource@axe312.dev>
2020-03-11 19:25:47 +0100

    fix(starters/contentful): improve vertical rythim

--- starters/contentful/src/gatsby-plugin-theme-ui/index.js
+++ starters/contentful/src/gatsby-plugin-theme-ui/index.js
@@ -2,4 +2,4 @@ import merge from 'lodash/merge'
 import defaultTheme from 'gatsby-theme-mdx-suite/src/gatsby-plugin-theme-ui'
 import typographyTheme from './typography'
 
-export default merge(defaultTheme, { fonts: typographyTheme.fonts })
+export default merge(defaultTheme, typographyTheme)
