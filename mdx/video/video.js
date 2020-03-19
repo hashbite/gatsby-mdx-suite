e29ff2641884e98629bc50c2ef87d5c41a7a97c4
Benedikt Rötsch <opensource@axe312.dev>
2020-03-19 18:11:31 +0100

    fix(mdx/video): add missing playsinline attribute for iOS

--- mdx/video/video.js
+++ mdx/video/video.js
@@ -98,6 +98,7 @@ export default function Video({
         onMouseEnter={handleVideoMouseEnter}
         onMouseLeave={handleVideoMouseLeave}
         controls={controls}
+        playsinline={autoplay || !controls}
         preload={preload}
         muted={autoplay || muted}
         poster={video.screenshots && video.screenshots[screenshotIndex]}
