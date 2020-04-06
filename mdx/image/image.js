c30a0a03e6091d030c2c45e97c330ac008b33e48
Benedikt Rötsch <opensource@axe312.dev>
2020-04-06 20:15:29 +0200

    fix(mdx/image): ensure image properly acts as static element by default

--- mdx/image/image.js
+++ mdx/image/image.js
@@ -39,13 +39,13 @@ export const ImageWrapper = styled('div', {
     }
 
     ${width &&
-      css`
-        width: ${parseCssValue(width)};
-      `}
+    css`
+      width: ${parseCssValue(width)};
+    `}
     ${height &&
-      css`
-        height: ${parseCssValue(height)};
-      `}
+    css`
+      height: ${parseCssValue(height)};
+    `}
   `
 )
 
@@ -156,7 +156,7 @@ export default function Image({
       <ImageWrapper fitsParent={fitsParent} {...dimensionProps} {...restProps}>
         <GatsbyImage
           fluid={fluid}
-          style={{ position: 'static' }}
+          style={{ position: fitsParent ? 'static' : 'relative' }}
           {...imgProps}
           {...dimensionProps}
           objectFit={fit}
