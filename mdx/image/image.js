9dd6e4cbf65705d5117cef90876db3a569971aeb
Benedikt Rötsch <opensource@axe312.dev>
2020-06-02 12:14:25 +0200

    fix(mdx/image): add fall backs when image is not available in current locale

--- mdx/image/image.js
+++ mdx/image/image.js
@@ -100,20 +100,41 @@ export default function Image({
   const {
     data,
     pageContext: { locale: activeLocale },
+    themeConfig: { defaultLocale },
   } = useContext(MdxSuiteContext)
 
   // Locate image data from context if id is passed
   if (id) {
-    const images = data[contextKey]
+    if (!data[contextKey]) {
+      console.error(
+        new Error(
+          `The media context "${contextKey}" does not exist or does not contain any data.`
+        )
+      )
+      return null
+    }
+
+    const images = data[contextKey].filter(Boolean)
     if (!images) {
       console.error(new Error(`No images available in context "${contextKey}"`))
       return null
     }
 
-    // Fetch data from context when an id was passed
-    const imageData = data[contextKey].find(
-      ({ assetId, locale }) => assetId === id && locale === activeLocale
-    )
+    // Get all available image data in all locales
+    const matches = images.filter((asset) => asset.assetId === id)
+
+    // Get data from active locale
+    let imageData = matches.find(({ locale }) => locale === activeLocale)
+
+    // Fall back to data with default locale
+    if (!imageData) {
+      imageData = matches.find(({ locale }) => locale === defaultLocale)
+    }
+
+    // Fall back to first available data from any locale
+    if (!imageData && matches.length) {
+      imageData = matches[0]
+    }
 
     if (!imageData) {
       console.error(
