7a5b4abd5697d967e17ebd0a46cb2a88d007483a
Benedikt Rötsch <opensource@axe312.dev>
2020-06-13 00:25:53 +0200

    refactor(starters): remove starter and demo content

--- starters/contentful/src/gatsby-theme-mdx-suite/gatsby-plugin-theme-ui/index.js
+++
@@ -1,7 +0,0 @@
-import defaultTheme from 'gatsby-theme-mdx-suite/src/gatsby-plugin-theme-ui'
-
-// Here you can alter the themes configuration to fit your projects need
-// Use lodash/merge to extend the default config with your own
-// The resulting theme config is available at /docs/theme
-
-export default defaultTheme
