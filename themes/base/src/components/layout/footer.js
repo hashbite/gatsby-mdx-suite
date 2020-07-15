aeee0193bb18a7736b444b6b2f391aa9467b222d
Benedikt Rötsch <opensource@axe312.dev>
2020-07-15 18:07:39 +0200

    feat: introduce simple default layout with header, navigation and footer

--- themes/base/src/components/layout/footer.js
+++ themes/base/src/components/layout/footer.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 import tw from 'twin.macro'
 
+import centerToContentColumn from '@gatsby-mdx-suite/helpers/styling/center-to-content-column'
+
 import MenuRecursive from '../menu/menu-recursive'
+import MenuUl from '../menu/menu-ul'
 
-import { useTranslation } from 'react-i18next'
-
-const FooterWrapper = tw.footer`py-8`
+const FooterWrapper = styled.footer(
+  (props) => css`
+    ${tw`py-8`}
+    ${centerToContentColumn(props)}
+  `
+)
+const FooterMenu = styled.div`
+  ${tw`py-8`}
+  ${MenuUl} {
+    ${tw`justify-center`}
+  }
+`
+const FooterCopy = tw.div`text-sm text-center`
 
 export default function Footer() {
   const { t } = useTranslation()
