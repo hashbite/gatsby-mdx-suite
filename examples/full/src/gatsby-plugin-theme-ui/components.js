2f641286b2a87887036e58b7690dd1f79b7b6c21
Benedikt Rötsch <opensource@axe312.dev>
2020-07-16 20:04:31 +0200

    feat: add new full example to demonstarte the blog and i18n

---
+++ examples/full/src/gatsby-plugin-theme-ui/components.js
@@ -0,0 +1,22 @@
+// Basics
+import Image from '@gatsby-mdx-suite/mdx-image/image'
+import Link from '@gatsby-mdx-suite/mdx-link/link'
+
+// Layout
+import Columns from '@gatsby-mdx-suite/mdx-layout/columns'
+import Column from '@gatsby-mdx-suite/mdx-layout/column'
+import Section from '@gatsby-mdx-suite/mdx-layout/section'
+
+// Project specific components
+import Header from 'gatsby-theme-mdx-suite-base/src/components/layout/header'
+import CustomComponent from '../components/custom-component'
+
+export default {
+  Column,
+  Columns,
+  Image,
+  Link,
+  Section,
+  Header,
+  CustomComponent,
+}
