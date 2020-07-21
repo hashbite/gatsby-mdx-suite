40f37f9916bd85f96b277827164f2ca43ae12384
Benedikt Rötsch <opensource@axe312.dev>
2020-07-21 17:00:36 +0200

    feat: add default content and migrations for assets, pages, menu and blog

---
+++ themes/blog/contentful/contentful-migration-blog.js
@@ -0,0 +1,136 @@
+module.exports = function (migration) {
+  const blogPost = migration
+    .createContentType('blogPost')
+    .name('Blog Post')
+    .description('')
+    .displayField('title')
+  blogPost
+    .createField('title')
+    .name('Title')
+    .type('Symbol')
+    .localized(true)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+
+  blogPost
+    .createField('slug')
+    .name('Slug')
+    .type('Symbol')
+    .localized(true)
+    .required(true)
+    .validations([
+      {
+        unique: true,
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+
+  blogPost
+    .createField('publicationDate')
+    .name('Publication Date')
+    .type('Date')
+    .localized(false)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+
+  blogPost
+    .createField('image')
+    .name('Image')
+    .type('Link')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        linkMimetypeGroup: ['image'],
+      },
+      {
+        assetImageDimensions: {
+          width: {
+            min: 1200,
+            max: null,
+          },
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .linkType('Asset')
+
+  blogPost
+    .createField('metaDescription')
+    .name('Meta Description')
+    .type('Symbol')
+    .localized(true)
+    .required(false)
+    .validations([
+      {
+        size: {
+          max: 155,
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+
+  blogPost
+    .createField('teaser')
+    .name('Teaser')
+    .type('Text')
+    .localized(true)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+
+  blogPost
+    .createField('content')
+    .name('Content')
+    .type('Text')
+    .localized(true)
+    .required(true)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+
+  blogPost.changeFieldControl('title', 'builtin', 'singleLine', {
+    helpText:
+      'Set a title for the browser tab, search engine results & social media shares. No longer than 60-70 characters.',
+  })
+
+  blogPost.changeFieldControl('slug', 'builtin', 'slugEditor', {
+    helpText:
+      'Set the URL identifier for this page. If you change this later one, you might harm your SEO score. The whole URL should have a maximum of 55-60 characters. This includes the domain name.',
+  })
+
+  blogPost.changeFieldControl('publicationDate', 'builtin', 'datePicker', {
+    ampm: '24',
+    format: 'timeZ',
+    helpText:
+      'Set the public publication date for this blog post. Will be displayed to the user and used for ordering.',
+  })
+
+  blogPost.changeFieldControl('image', 'builtin', 'assetLinkEditor', {
+    helpText:
+      'This image is used for teasers, when this blog post is shared on search machines and might be shown in the header of the detail page.',
+  })
+
+  blogPost.changeFieldControl('metaDescription', 'builtin', 'singleLine', {
+    helpText:
+      'Description which will be used on search engine result pages and when this page is shared in social media.',
+  })
+
+  blogPost.changeFieldControl('teaser', 'builtin', 'markdown', {
+    helpText:
+      'Teaser/preview content, especially used on the blog post overview page. Use MDX syntax to add layout and design to the page. See the documentation of the project for more information about MDX.',
+  })
+
+  blogPost.changeFieldControl('content', 'builtin', 'markdown', {
+    helpText:
+      'The actual content. Use MDX syntax to add layout and design to the page. See the documentation of the project for more information about MDX.',
+  })
+}
---
+++ themes/core/contentful/contentful-migration-menu.js
@@ -0,0 +1,114 @@
+module.exports = function (migration) {
+  const menuItem = migration
+    .createContentType('menuItem')
+    .name('Menu Item')
+    .description('')
+    .displayField('title')
+  menuItem
+    .createField('title')
+    .name('Title')
+    .type('Symbol')
+    .localized(true)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+
+  menuItem
+    .createField('linkedPage')
+    .name('Linked Page')
+    .type('Link')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        linkContentType: ['page'],
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+    .linkType('Entry')
+
+  menuItem
+    .createField('externalUri')
+    .name('External URI')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        regexp: {
+          pattern:
+            '^\\w+:\\/\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\/|\\/([\\w#!:.?+=&%@!\\-\\/]))?$',
+        },
+      },
+    ])
+    .disabled(false)
+    .omitted(false)
+
+  menuItem
+    .createField('internalSlug')
+    .name('Internal slug')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+
+  menuItem
+    .createField('subitems')
+    .name('Subitems')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Link',
+
+      validations: [
+        {
+          linkContentType: ['menuItem'],
+        },
+      ],
+
+      linkType: 'Entry',
+    })
+
+  menuItem
+    .createField('openInNewTab')
+    .name('Open in new tab')
+    .type('Boolean')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .disabled(false)
+    .omitted(false)
+  menuItem.changeFieldControl('title', 'builtin', 'singleLine', {})
+
+  menuItem.changeFieldControl('linkedPage', 'builtin', 'entryLinkEditor', {
+    helpText: 'Actual page this menu item is linking to',
+  })
+
+  menuItem.changeFieldControl('externalUri', 'builtin', 'urlEditor', {
+    helpText: 'Link an external page.',
+  })
+
+  menuItem.changeFieldControl('internalSlug', 'builtin', 'singleLine', {
+    helpText:
+      'Link hardcoded internal projects. Only use as last resort, always prefer to link pages.',
+  })
+
+  menuItem.changeFieldControl('subitems', 'builtin', 'entryLinksEditor', {
+    bulkEditing: true,
+  })
+
+  menuItem.changeFieldControl('openInNewTab', 'builtin', 'boolean', {
+    helpText:
+      'Open link in a new browser tab. Should only be used for edge-cases. See https://css-tricks.com/use-target_blank/',
+    trueLabel: 'Yes',
+    falseLabel: 'No',
+  })
+}
