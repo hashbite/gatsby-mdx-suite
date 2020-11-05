66050265b06cef821ef7e64b81dbdbf9393abc68
Benedikt Rötsch <opensource@axe312.dev>
2020-11-05 17:14:12 +0100

    feat(responsive-font): support rem values for Claim, ResponsiveTextBlock and more

--- packages/helpers/styling/responsive-font.js
+++ packages/helpers/styling/responsive-font.js
@@ -2,28 +2,34 @@ import { css } from '@emotion/core'
 
 const responsiveFont = ({
   theme,
-  fontSizeMin = '24px',
-  fontSizeMax = '64px',
-  lineHeightMin = '1.4em',
-  lineHeightMax = '1.1em',
+  fontSizeMin = '1.5rem',
+  fontSizeMax = '4rem',
+  lineHeightMin = '1.1',
+  lineHeightMax = '1.3',
 }) => {
   const scaleStart = '300px'
   const scaleEnd = theme.spacing['content-column']
   const scaleRange = parseInt(scaleEnd) - parseInt(scaleStart)
+
+  let diff = parseFloat(fontSizeMax) - parseFloat(fontSizeMin)
+
+  if (fontSizeMin.indexOf('rem') !== -1) {
+    fontSizeMin = `${parseFloat(fontSizeMin) * 16}px`
+    fontSizeMax = `${parseFloat(fontSizeMax) * 16}px`
+    diff = diff * 16
+  }
 
   return css`
     font-size: ${fontSizeMin};
     line-height: ${lineHeightMin};
 
     @media screen and (min-width: ${scaleStart}) {
-      font-size: calc(
-        ${fontSizeMin} + (${parseFloat(fontSizeMax) - parseFloat(fontSizeMin)}) *
-          ((100vw - ${scaleStart}) / ${scaleRange})
-      );
+      --scale-factor: calc(100vw - ${scaleStart}) / ${scaleRange};
+      font-size: calc(${fontSizeMin} + ${diff} * var(--scale-factor));
       line-height: calc(
         ${lineHeightMin} +
           (${parseFloat(lineHeightMax) - parseFloat(lineHeightMin)}) *
-          ((100vw - ${scaleStart}) / ${scaleRange})
+          var(--scale-factor)
       );
     }
     @media screen and (min-width: ${scaleEnd}) {
