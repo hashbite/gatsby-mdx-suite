949c1b6652d0262e8298385763d3299d67001dc3
Benedikt Rötsch <opensource@axe312.dev>
2020-11-05 15:22:11 +0100

    fix(docs): ensure Header and Hide are parsed for component list

--- themes/docs/gatsby-config.js
+++ themes/docs/gatsby-config.js
@@ -7,9 +7,17 @@ const debug = Debug('gatsby-theme-mdx-suite-docs')
 const defaultComponentPaths = [
   // Project/custom components
   resolve(process.cwd(), `src/components/mdx/`),
+  // Base theme components
+  resolve(
+    process.cwd(),
+    `node_modules/gatsby-theme-mdx-suite-base/src/components/mdx/`
+  ),
+  resolve(
+    process.cwd(),
+    `../../node_modules/gatsby-theme-mdx-suite-base/src/components/mdx/`
+  ),
   // External components
   resolve(process.cwd(), `node_modules/@gatsby-mdx-suite/`),
-  // Lerna/yarn workspaces environment
   resolve(process.cwd(), `../../node_modules/@gatsby-mdx-suite/`),
 ]
 
