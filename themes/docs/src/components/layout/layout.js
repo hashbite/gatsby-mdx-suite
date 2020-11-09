1beac0e53f4eceb05c633501f243aa6df23e5c95
Benedikt Rötsch <opensource@axe312.dev>
2020-11-09 15:56:55 +0100

    fix(docs): improve docs layout spacing

--- themes/docs/src/components/layout/layout.js
+++ themes/docs/src/components/layout/layout.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Link from 'gatsby-link'
 import propTypes from 'prop-types'
 import styled from '@emotion/styled'
 import { css } from '@emotion/core'
 import tw from 'twin.macro'
 
-import MenuIcon from 'heroicons/outline/menu.svg'
-import ChevronLeftIcon from 'heroicons/outline/chevron-left.svg'
+import Icon from '@gatsby-mdx-suite/mdx-copy/icon'
 
 const DocsWrapper = styled.div`
   ${tw`grid h-screen w-screen`}
@@ -18,56 +17,41 @@ const DocsWrapper = styled.div`
 
 const LayoutHeader = styled.div`
   ${tw`
-    p-4 flex justify-between items-center flex-wrap
-    md:py-0 md:flex-no-wrap
+    p-4 flex justify-between items-center flex-no-wrap
     shadow-lg
   `}
   grid-area: head;
 `
 
-const DocsTitle = tw.h1`m-0 text-2xl`
+const DocsTitle = tw.h1`mb-0! text-2xl`
 
-const MenuLink = tw(Link)`my-2 md:my-0 md:px-2 lg:px-4`
+const MenuLink = tw(Link)`px-2 lg:px-4`
 
 const Menu = styled.nav(
-  ({ menuOpen }) => css`
+  () => css`
     ${tw`
-    py-1 w-full flex-grow
-    md:py-0 md:w-auto md:flex-auto
-    flex flex-col content-center
-    md:flex-initial md:flex-row
-    md:text-sm lg:text-base
+    flex-auto
+    flex flex-row content-center
+    flex-initial
+    text-sm lg:text-base
   `}
-    ${!menuOpen && tw`hidden md:flex md:justify-end`}
   `
 )
 
-const MenuToggleWrapper = tw.div`p-1 md:hidden`
-const MenuToggleIcon = tw(MenuIcon)`cursor-pointer w-8`
-const BackIcon = tw(
-  ChevronLeftIcon
-)`cursor-pointer w-6 inline align-middle pb-1`
+const BackIcon = tw(Icon)`cursor-pointer w-6 align-middle pb-1`
 
 const Layout = ({ children, title }) => {
-  const [menuOpen, setMenuOpen] = useState(false)
-
-  const handleMenuToggleClick = () => {
-    setMenuOpen(!menuOpen)
-  }
   return (
     <DocsWrapper>
       <LayoutHeader>
         <DocsTitle>
           <MenuLink to="/">
-            <BackIcon />
+            <BackIcon icon="previous" />
           </MenuLink>
-          <MenuLink to="/docs">Docs</MenuLink>
+          <MenuLink to="/docs">Docs{title && ` - ${title}`}</MenuLink>
         </DocsTitle>
-        <MenuToggleWrapper>
-          <MenuToggleIcon onClick={handleMenuToggleClick} />
-        </MenuToggleWrapper>
-        <Menu menuOpen={menuOpen}>
-          <MenuLink to="/docs">Home</MenuLink>
+        <Menu>
+          <MenuLink to="/docs">Start</MenuLink>
           <MenuLink to="/docs/playground">Playground</MenuLink>
           <MenuLink to="/docs/components">Components</MenuLink>
           <MenuLink to="/docs/theme">Theme</MenuLink>
