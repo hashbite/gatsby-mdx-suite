a8596132ddb7775c1b8b3ed1094f74e8b03823e8
Benedikt Rötsch <opensource@axe312.dev>
2020-11-09 15:07:25 +0100

    feat(icons): improve icon placement and add property for vertical alignment

--- mdx/copy/icon.js
+++ mdx/copy/icon.js
@@ -7,10 +7,15 @@ import IconsContext from '@gatsby-mdx-suite/contexts/icons'
 import selectColor from '@gatsby-mdx-suite/helpers/styling/select-color'
 
 const IconWrapper = styled.span(
-  ({ scale, color, theme }) =>
+  ({ verticalAlign, scale, color, theme }) =>
     css`
       display: inline-block;
       transform-origin: 50% 50%;
+
+      ${verticalAlign !== 'baseline' &&
+      css`
+        vertical-align: ${verticalAlign};
+      `}
 
       ${color &&
       css`
@@ -23,6 +28,7 @@ const IconWrapper = styled.span(
       `}
 
       svg {
+        display: inline-block;
         width: 1em;
         height: auto;
       }
@@ -47,14 +53,22 @@ const Icon = ({ icon, color, svgProps, ...props }) => {
   const ActualIcon = IconData.icon
   return (
     <IconWrapper scale={IconData.scale} color={color} {...props}>
-      <ActualIcon valign="baseline" {...svgProps} />
+      <ActualIcon {...svgProps} />
     </IconWrapper>
   )
 }
 
+Icon.defaultProps = {
+  verticalAlign: 'baseline',
+}
+
 Icon.propTypes = {
+  /** Name of the icon to render. The available icons can be found in the <Link to="/docs/theme#icons">theme documentation for icons</Link> */
   icon: propTypes.string.isRequired,
+  /** Color of the icon. Will use current text color by default.*/
   color: propTypes.string,
+  /** Adjust the vertical align of the icon. Useful values: top, bottom, middle, baseline, text-top, text-bottom */
+  verticalAlign: propTypes.string,
   svgProps: propTypes.object,
 }
 
