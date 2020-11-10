e096371f5ffd78f2a0b9d99152990ab4c0a8a8d7
Benedikt Rötsch <opensource@axe312.dev>
2020-11-10 15:12:39 +0100

    fix(layout): use reach router hook to check for docs

--- themes/base/src/components/layout/layout.js
+++ themes/base/src/components/layout/layout.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import styled from '@emotion/styled'
+import { useLocation } from '@reach/router'
 
 import Footer from './footer'
 import Seo from './seo'
 
 const Wrapper = styled.div``
 
-const Layout = ({ children, uri }) => {
-  const isDocs = uri.indexOf('/docs') === 0
+const Layout = ({ children }) => {
+  const location = useLocation()
+  const isDocs = location.pathname.indexOf('/docs') === 0
+
   return (
     <>
       <Seo />
