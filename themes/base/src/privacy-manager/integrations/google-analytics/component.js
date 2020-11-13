ea1f80f3b6bd0fd3f63bdb949ae37b6de9cff64f
Benedikt Rötsch <opensource@axe312.dev>
2020-11-13 14:04:52 +0100

    fix(privacy-manager): allow GA and GTM trackers to be configured

--- themes/base/src/privacy-manager/integrations/google-analytics/component.js
+++ themes/base/src/privacy-manager/integrations/google-analytics/component.js
@@ -1,18 +1,25 @@
 import { useEffect, useState } from 'react'
+import propTypes from 'prop-types'
+
 import ReactGA from 'react-ga'
 
-const GoogleTagAnalytics = ({ children }) => {
+const GoogleTagAnalytics = ({ children, trackingId }) => {
   const [initialized, setInitialized] = useState(false)
   useEffect(() => {
     if (!initialized) {
-      ReactGA.initialize('UA-XXXXXXX-1')
+      ReactGA.initialize(trackingId)
       ReactGA.pageview(window.location.pathname + window.location.search)
       window.ga = ReactGA.ga()
       window.ReactGA = ReactGA
       setInitialized(true)
     }
-  }, [initialized])
+  }, [initialized, trackingId])
+
   return children
+}
+
+GoogleTagAnalytics.propTypes = {
+  trackingId: propTypes.string.isRequired,
 }
 
 export default GoogleTagAnalytics
