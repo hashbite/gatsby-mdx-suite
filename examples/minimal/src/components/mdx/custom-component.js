166a3508073a036b04a39f4a7ec6321b4c8ce4d5
Benedikt Rötsch <opensource@axe312.dev>
2020-12-01 18:18:03 +0100

    refactor: move custom component example into minimal

--- examples/minimal/src/components/mdx/custom-component.js
+++ examples/minimal/src/components/mdx/custom-component.js
@@ -4,12 +4,28 @@ import tw from 'twin.macro'
 
 const CustomComponentWrapper = tw.div`bg-red-600 h-12`
 
+/**
+ * Description for your custom component
+ *
+ * @example
+ * <CustomComponent>
+ *
+ * # Example headline
+ *
+ * </CustomComponent>
+ */
 const CustomComponent = ({ children }) => {
   return <CustomComponentWrapper>{children}</CustomComponentWrapper>
 }
 
+CustomComponent.defaultProps = {
+  doSomething: 'nah',
+}
+
 CustomComponent.propTypes = {
   children: propTypes.node,
+  /** Description for your property */
+  doSomething: propTypes.string,
 }
 
 export default CustomComponent
