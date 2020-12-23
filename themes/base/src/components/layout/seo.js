e3b52ea71289859a209dbfcd787867743a9a451c
Benedikt Rötsch <opensource@axe312.dev>
2020-12-23 13:09:44 +0100

    perf(metadata): replace react-helmet with smaller hoofd

--- themes/base/src/components/layout/seo.js
+++ themes/base/src/components/layout/seo.js
@@ -1,8 +1,7 @@
-import React from 'react'
 import PropTypes from 'prop-types'
-import { Helmet } from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
-import { Location } from '@reach/router'
+import { useLocation } from '@reach/router'
+import { useHead } from 'hoofd'
 
 function SEO({ description, lang, meta, title, ogImage, twitterImage, url }) {
   const { site } = useStaticQuery(
@@ -21,80 +20,75 @@ function SEO({ description, lang, meta, title, ogImage, twitterImage, url }) {
 
   const metaDescription = description || site.siteMetadata.description
   const metaUrl = url || site.siteMetadata.siteUrl
+  const location = useLocation()
 
-  return (
-    <Location>
-      {({ location }) => (
-        <Helmet
-          htmlAttributes={{
-            lang,
-          }}
-          title={title}
-          titleTemplate={`%s | ${site.siteMetadata.title}`}
-          meta={[
-            {
-              name: `description`,
-              content: metaDescription,
-            },
-            {
-              property: `og:title`,
-              content: title,
-            },
-            {
-              property: `og:description`,
-              content: metaDescription,
-            },
-            {
-              property: `og:type`,
-              content: `website`,
-            },
-            {
-              property: 'og:url',
-              content: `${metaUrl}/${location.pathname}`,
-            },
-            ogImage && {
-              property: 'og:image',
-              content: ogImage,
-            },
-            {
-              name: `twitter:card`,
-              content: `summary`,
-            },
-            {
-              name: `twitter:title`,
-              content: title,
-            },
-            {
-              name: `twitter:description`,
-              content: metaDescription,
-            },
-            twitterImage && {
-              property: 'twitter:image:src',
-              content: twitterImage,
-            },
-            {
-              name: 'viewport',
-              content: 'width=device-width, initial-scale=1',
-            },
-            {
-              name: 'apple-mobile-web-app-capable',
-              content: 'yes',
-            },
-            {
-              name: 'apple-mobile-web-app-status-bar-style',
-              content: 'black-translucent',
-            },
-            {
-              name: 'format-detection',
-              content: 'telephone=no',
-            },
-          ]
-            .filter(Boolean)
-            .concat(meta)}
-        />
-      )}
-    </Location>
-  )
+  useHead({
+    title,
+    language: lang,
+    titleTemplate: `%s | ${title}`,
+    metas: [
+      {
+        name: `description`,
+        content: metaDescription,
+      },
+      {
+        property: `og:title`,
+        content: title,
+      },
+      {
+        property: `og:description`,
+        content: metaDescription,
+      },
+      {
+        property: `og:type`,
+        content: `website`,
+      },
+      {
+        property: 'og:url',
+        content: `${metaUrl}/${location.pathname}`,
+      },
+      ogImage && {
+        property: 'og:image',
+        content: ogImage,
+      },
+      {
+        name: `twitter:card`,
+        content: `summary`,
+      },
+      {
+        name: `twitter:title`,
+        content: title,
+      },
+      {
+        name: `twitter:description`,
+        content: metaDescription,
+      },
+      twitterImage && {
+        property: 'twitter:image:src',
+        content: twitterImage,
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        name: 'apple-mobile-web-app-capable',
+        content: 'yes',
+      },
+      {
+        name: 'apple-mobile-web-app-status-bar-style',
+        content: 'black-translucent',
+      },
+      {
+        name: 'format-detection',
+        content: 'telephone=no',
+      },
+    ]
+      .filter(Boolean)
+      .concat(meta),
+  })
+
+  return null
 }
 
 SEO.defaultProps = {
--- themes/base/src/privacy-manager/integrations/google-analytics/route-updater.js
+++ themes/base/src/privacy-manager/integrations/google-analytics/route-updater.js
@@ -13,6 +13,7 @@ export const onRouteUpdate = ({ location }, pluginOptions = {}) => {
 
   // wrap inside a timeout to make sure react-helmet is done with it's changes (https://github.com/gatsbyjs/gatsby/issues/9139)
   // reactHelmet is using requestAnimationFrame: https://github.com/nfl/react-helmet/blob/5.2.0/src/HelmetUtils.js#L296-L299
+  // @todo is this still needed as we use hoofd now?
   const sendPageView = () => {
     const pagePath = location
       ? location.pathname + location.search + location.hash
