eeebab95051d7f2c694969b94dd4bdc9dd7c51f0
Benedikt Rötsch <opensource@axe312.dev>
2021-02-07 17:43:37 +0100

    fix(forms): improve buttons

--- themes/base/src/components/form/fields/button.js
+++ themes/base/src/components/form/fields/button.js
@@ -6,15 +6,15 @@ import { focusStyle } from './styles'
 const Button = styled.button`
   ${tw`rounded bg-gray-200 text-gray-900 px-2 py-1`}
 
+  :active {
+    ${tw`bg-gray-400`}
+  }
+
   :disabled {
     ${tw`text-gray-600`}
   }
 
   ${focusStyle}
-
-  & + & {
-    margin-left: 1rem;
-  }
 `
 
 export default Button
