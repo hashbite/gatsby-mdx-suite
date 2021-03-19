9382c96e6458383f10f0d68ffeecf8c4b49d02e5
Benedikt Rötsch <opensource@axe312.dev>
2021-03-19 15:28:54 +0100

    migrate to new trackPageViewSPA

--- themes/base/src/consent-manager/on-route-update.js
+++ themes/base/src/consent-manager/on-route-update.js
@@ -8,32 +8,23 @@ const { getMatomoTracker } = require('@consent-manager/integration-matomo')
 module.exports = (function () {
   return {
     onRouteUpdate({ location, prevLocation }) {
-      const { track } = getMatomoTracker()
-
-      const url =
-        location && location.pathname + location.search + location.hash
-      const prevUrl =
-        prevLocation &&
-        prevLocation.pathname + prevLocation.search + prevLocation.hash
-
-      // @todo this cound be a helper function in the getMatomoTracker for SPAs
-      const sendPageView = () => {
-        const { title } = document
-
-        prevUrl && track('setReferrerUrl', prevUrl)
-        track('setCustomUrl', url)
-        track('setDocumentTitle', title)
-        track('trackPageView')
-        track('enableLinkTracking')
-        track('trackAllContentImpressions')
-
-        if (process.env.gatsby_log_level === `verbose`) {
-          console.debug(`[Matomo] Page view for: ${url} - ${title}`)
-        }
-      }
+      const { trackPageViewSPA } = getMatomoTracker()
 
       // This ensures plugins like react-helmet finished their work
-      window.setTimeout(sendPageView, 0)
+      window.setTimeout(() => {
+        const trackResult = trackPageViewSPA({ location, prevLocation })
+
+        // Debug logging
+        if (process.env.gatsby_log_level === `verbose`) {
+          const { url, title } = trackResult
+          if (!trackResult) {
+            return console.debug(
+              `[Matomo] Failed to track page view: ${url} - ${title}`
+            )
+          }
+          console.debug(`[Matomo] Page view for: ${url} - ${title}`)
+        }
+      }, 0)
     },
   }
 })()
