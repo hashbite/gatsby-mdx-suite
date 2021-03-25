f12ee9567e95b3aefbcf5bae57239bb1ede58575
Benedikt Rötsch <opensource@axe312.dev>
2021-03-25 11:43:55 +0100

    fix: improve consent-manager integration

--- packages/helpers/hooks/use-consent-manager-config.js
+++ packages/helpers/hooks/use-consent-manager-config.js
@@ -7,8 +7,13 @@ export const useConsentManagerConfig = (components, userConfig) => {
   // Add integration from component when not defined by user
   Object.keys(components).forEach((key) => {
     const integration = components[key]?.privacy
-    console.log({ com: components[key], integration })
     if (integration && !uniqueIntegrations.has(integration.id)) {
+      if (process.env.gatsby_log_level === `verbose`) {
+        console.log('Located integration from MDX component:', {
+          component: components[key],
+          integration,
+        })
+      }
       uniqueIntegrations.set(integration.id, integration)
     }
   })
