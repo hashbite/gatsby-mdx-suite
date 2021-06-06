4ec212a54ed66f7321c4cd02ea2e9a4911b8f5ea
axe312ger <opensource@axe312.dev>
2021-06-06 22:04:33 +0200

    refactor: show iconset manipulaton in full example only

--- examples/full/src/icons.js
+++ examples/full/src/icons.js
@@ -1,11 +1,13 @@
 import defaultIcons from 'gatsby-theme-mdx-suite-base/src/default-icons'
 
 import { SiGithub } from '@react-icons/all-files/si/SiGithub'
+import GatsbyMonogram from './assets/icons/gatsby-monogram.svg'
 
 export default new Map([
   ...defaultIcons,
   // Project specific icons
   ...Object.entries({
     github: { icon: SiGithub },
+    gatsby: { icon: GatsbyMonogram },
   }),
 ])
