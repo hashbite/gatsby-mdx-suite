851fd79f6deb8fb3d836ccd7753f6675f6b83cd8
axe312ger <opensource@axe312.dev>
2021-06-06 22:04:33 +0200

    refactor: show iconset manipulaton in full example only

--- examples/full/src/gatsby-theme-mdx-suite-base/icons.js
+++
@@ -1,5 +0,0 @@
-import icons from 'gatsby-theme-mdx-suite-base/src/icons'
-
-import GatsbyMonogram from '../assets/icons/gatsby-monogram.svg'
-
-export default { ...icons, gatsby: { icon: GatsbyMonogram } }
