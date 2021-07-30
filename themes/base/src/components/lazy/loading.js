a88e70e4ce85a83a13dba4c5c395d99cacdb5f81
axe312ger <opensource@axe312.dev>
2021-07-30 14:43:42 +0200

    feat(lazy): improve style of default loading indicator

--- themes/base/src/components/lazy/loading.js
+++ themes/base/src/components/lazy/loading.js
@@ -1,45 +1,74 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import styled from '@emotion/styled'
-import { css } from '@emotion/react'
+import { css, keyframes } from '@emotion/react'
 import tw from 'twin.macro'
 
 const DefaultLoadingWrapper = styled.div(
-  ({ aspectRatio }) => css`
+  () => css`
     ${tw`
-      w-full flex items-center justify-center
-      p-content-gap
-      border border-solid border-4 border-gray-800
-      bg-gray-700 text-white text-2xl
+      w-full flex flex-col items-center justify-center
+      px-content-gap
+      py-16
     `}
-
-    &::before {
-      content: '';
-      width: 1px;
-      margin-left: -1px;
-      float: left;
-      height: 0;
-      padding-top: calc(100% / (${aspectRatio}));
-    }
-    &::after {
-      content: '';
-      display: table;
-      clear: both;
-    }
   `
 )
 
-const LoadingPlaceholder = ({ aspectRatio }) => {
+const WaveAnimation = keyframes`
+  0%, 40%, 100% {
+    transform: scaleY(0.4);
+  } 20% {
+    transform: scaleY(1);
+  }
+`
+
+const Label = styled.div`
+  ${tw`mt-4`}
+`
+const WaveWrapper = styled.div`
+  ${tw`w-20 h-8`}
+  display: flex;
+  justify-content: space-between;
+`
+const Wave = styled.div`
+  ${tw`bg-primary`}
+  height: 100%;
+  width: 15%;
+  animation: ${WaveAnimation} 1.2s infinite ease-in-out;
+
+  &:nth-child(1) {
+    animation-delay: -1.2s;
+  }
+  &:nth-child(2) {
+    animation-delay: -1.1s;
+  }
+  &:nth-child(3) {
+    animation-delay: -1s;
+  }
+  &:nth-child(4) {
+    animation-delay: -0.9s;
+  }
+  &:nth-child(5) {
+    animation-delay: -0.8s;
+  }
+`
+
+const LoadingPlaceholder = ({ labelText = 'Loading...' }) => {
   return (
-    <DefaultLoadingWrapper aspectRatio={aspectRatio}>
-      Loading...
+    <DefaultLoadingWrapper>
+      <WaveWrapper>
+        <Wave />
+        <Wave />
+        <Wave />
+        <Wave />
+        <Wave />
+      </WaveWrapper>
+      <Label>{labelText}</Label>
     </DefaultLoadingWrapper>
   )
 }
 
-LoadingPlaceholder.defaultProps = {
-  aspectRatio: '16 / 9',
-}
+LoadingPlaceholder.defaultProps = {}
 
 LoadingPlaceholder.propTypes = {
   aspectRatio: propTypes.string,
