5c97c2237840d74bc36354e676896b20c87d5d0c
axe312ger <opensource@axe312.dev>
2021-08-17 17:30:40 +0200

    perf: replace GSAP in NavBar with IntersectionObserver

--- themes/base/src/components/navigation/bar.js
+++ themes/base/src/components/navigation/bar.js
@@ -1,19 +1,14 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react'
 import propTypes from 'prop-types'
 import styled from '@emotion/styled'
-import { Global, css } from '@emotion/react'
+import { Global, css, useTheme } from '@emotion/react'
 import tw from 'twin.macro'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { useTheme } from '@emotion/react'
+import { useIntersection, useWindowSize } from 'react-use'
 
 import selectColor from '@gatsby-mdx-suite/helpers/styling/select-color'
 
 import NavigationDesktop from './desktop'
 import NavigationMobile from './mobile'
-import { useKillScrollTriggerOnCleanup } from '@gatsby-mdx-suite/helpers/styling/gsap'
-
-gsap.registerPlugin(ScrollTrigger)
 
 const NavigationBarWrapper = styled.div(
   ({
@@ -22,11 +17,12 @@ const NavigationBarWrapper = styled.div(
     background,
     transparentTextColor,
     transparentBackground,
+    isFloating,
   }) => css`
     ${tw`shadow-xl`}
     z-index: 100;
 
-    ${transparent
+    ${transparent && !isFloating
       ? css`
           ${tw`absolute left-0 top-0 w-full`}
           ${tw`text-white`}
@@ -37,6 +33,8 @@ const NavigationBarWrapper = styled.div(
           color: ${textColor};
           background: ${background};
         `};
+
+    ${isFloating && tw`fixed left-0 top-0 right-0`}
   `
 )
 
@@ -48,92 +46,77 @@ const NavigationBar = ({
   transparentTextColor,
   transparentBackground,
 }) => {
+  const theme = useTheme()
+  const { width } = useWindowSize()
+
   const [headerHeight, setHeaderHeight] = useState(0)
   const [menuOpen, setMenuOpen] = useState(false)
-  const theme = useTheme()
-  const [scrollTriggerInstancePinning, setScrollTriggerInstancePinning] =
-    useState(null)
-  const [scrollTriggerInstanceFade, setScrollTriggerInstanceFade] =
-    useState(null)
+  const [positionParentNodeRef, setPositionParentNodeRef] = useState({})
+  const [nodeRef, setNodeRef] = useState(null)
+  const spacerRef = useRef(null)
 
-  useKillScrollTriggerOnCleanup(scrollTriggerInstancePinning)
-  useKillScrollTriggerOnCleanup(scrollTriggerInstanceFade)
+  const handleRef = useCallback(
+    (node) => {
+      if (!node) {
+        return
+      }
+      setNodeRef({ current: node })
+      setPositionParentNodeRef({
+        current: transparent ? node.closest('section') : spacerRef.current,
+      })
+    },
+    [transparent]
+  )
+
+  const intersection = useIntersection(positionParentNodeRef, {
+    threshold: 0.1,
+  })
+
+  const isFloating = useMemo(() => {
+    if (!intersection) {
+      return
+    }
+    const shouldFloat =
+      intersection &&
+      intersection.intersectionRatio <= 0.1 &&
+      intersection.boundingClientRect.top <= 0
+
+    return shouldFloat
+  }, [intersection])
+
+  useEffect(() => {
+    if (nodeRef && !isFloating) {
+      setHeaderHeight(nodeRef.current.offsetHeight)
+    }
+  }, [isFloating, width, nodeRef])
 
   background = selectColor(theme.colors, background)
   textColor = selectColor(theme.colors, textColor)
   transparentTextColor = selectColor(theme.colors, transparentTextColor)
   transparentBackground = selectColor(theme.colors, transparentBackground)
 
-  const initScrollTrigger = useCallback(
-    (node) => {
-      if (!node) {
-        return
-      }
-      const positionParent = node.closest('section,#___gatsby')
-
-      if (sticky) {
-        setHeaderHeight(node.offsetHeight)
-
-        setScrollTriggerInstancePinning(
-          ScrollTrigger.create({
-            trigger: node,
-            endTrigger: 'body',
-            end: 'bottom bottom-=1',
-            pin: true,
-            pinSpacing: false,
-            pinType: 'fixed',
-            invalidateOnRefresh: true,
-            onLeaveBack: () => {
-              // Fix a bug in iOS safari which will draw the sticky element
-              // above viewport if you do the pull refresh gesture
-              node.style.transform = null
-            },
-          })
-        )
-
-        // Switch back to non-transparent rendering as soon we leave the parent section
-        if (transparent) {
-          setScrollTriggerInstanceFade(
-            gsap
-              .timeline({
-                scrollTrigger: {
-                  trigger: positionParent,
-                  start: `bottom-=${node.clientHeight * 2} top`,
-                  end: `bottom-=${node.clientHeight} top`,
-                  scrub: true,
-                  // toggleActions: 'play none reverse none',
-                  invalidateOnRefresh: true,
-                },
-              })
-              .to(node, {
-                color: textColor,
-                backgroundColor: background,
-              })
-          )
-        }
-      }
-    },
-    [sticky, transparent, background, textColor]
-  )
   return (
-    <NavigationBarWrapper
-      transparent={transparent}
-      ref={initScrollTrigger}
-      background={background}
-      textColor={textColor}
-      transparentTextColor={transparentTextColor}
-      transparentBackground={transparentBackground}
-    >
-      <NavigationDesktop />
-      <NavigationMobile menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Global
-        styles={css`
-          :root {
-            --floating-header-height: ${headerHeight}px;
-          }
-        `}
-      />
-    </NavigationBarWrapper>
+    <div style={{ minHeight: !transparent && headerHeight }} ref={spacerRef}>
+      <NavigationBarWrapper
+        transparent={transparent}
+        isFloating={isFloating}
+        ref={handleRef}
+        background={background}
+        textColor={textColor}
+        transparentTextColor={transparentTextColor}
+        transparentBackground={transparentBackground}
+      >
+        <NavigationDesktop />
+        <NavigationMobile menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Global
+          styles={css`
+            :root {
+              --floating-header-height: ${headerHeight}px;
+            }
+          `}
+        />
+      </NavigationBarWrapper>
+    </div>
   )
 }
 
