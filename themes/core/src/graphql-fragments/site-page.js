58e8e78fc32a3da082cddb8eaeac8d3d26c60737
axe312ger <opensource@axe312.dev>
2021-11-09 15:57:50 +0100

    build: migrate core code to Gatsby v4

--- themes/core/src/graphql-fragments/site-page.js
+++ themes/core/src/graphql-fragments/site-page.js
@@ -3,11 +3,6 @@ import { graphql } from 'gatsby'
 export const MdxSuiteSitePageMetadataFragment = graphql`
   fragment MdxSuiteSitePageMetadata on SitePage {
     path
-    context {
-      pageId
-      locale
-      title
-      pageNumber
-    }
+    pageContext
   }
 `
