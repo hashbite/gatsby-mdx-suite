b1b9bbff40160cb3397a0365df7348b197df333c
axe312ger <opensource@axe312.dev>
2022-02-21 13:45:50 +0100

    perf: speed up consent-manager integration

--- packages/helpers/hooks/use-consent-manager-config.js
+++ packages/helpers/hooks/use-consent-manager-config.js
@@ -1,24 +1,28 @@
+import { useMemo } from 'react'
+
 // Enhance users configuration with required integrations from MDX components
 export const useConsentManagerConfig = (components, userConfig) => {
   const uniqueIntegrations = new Map(
     (userConfig.integrations || []).map((i) => [i.id, i])
   )
 
-  // Add integration from component when not defined by user
-  Object.keys(components).forEach((key) => {
-    const integration = components[key]?.privacy
-    if (integration && !uniqueIntegrations.has(integration.id)) {
-      if (process.env.gatsby_log_level === `verbose`) {
-        console.log('Located integration from MDX component:', {
-          component: components[key],
-          integration,
-        })
+  return useMemo(() => {
+    // Add integration from component when not defined by user
+    Object.keys(components).forEach((key) => {
+      const integration = components[key]?.privacy
+      if (integration && !uniqueIntegrations.has(integration.id)) {
+        if (process.env.gatsby_log_level === `verbose`) {
+          console.log('Located integration from MDX component:', {
+            component: components[key],
+            integration,
+          })
+        }
+        uniqueIntegrations.set(integration.id, integration)
       }
-      uniqueIntegrations.set(integration.id, integration)
-    }
-  })
+    })
 
-  const integrations = [...uniqueIntegrations.values()]
+    const integrations = [...uniqueIntegrations.values()]
 
-  return { ...userConfig, integrations }
+    return { ...userConfig, integrations }
+  }, [Object.keys(components), uniqueIntegrations.keys()])
 }
